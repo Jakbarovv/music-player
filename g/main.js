0be28e45be07ab88bb3b8434a1ec9ae98d1ecfa9
Muhammadrasul <87309823+Jakbarovv@users.noreply.github.com>
2023-05-16 18:38:51 +0500

    gt

---
+++ g/main.js
@@ -0,0 +1,50 @@
+// console.log(8+5)
+
+// let id
+// console.log(typeof id)
+
+// id = 45
+// console.log(typeof id)
+
+// let x = 5;
+// let z = ++x;
+// console.log(x);
+// console.log(z);
+
+// let a = 5;
+// let b = ++a;
+// console.log(a);
+// console.log(b);
+
+// let g = 5;
+// let f = --g;
+// console.log(g);
+// console.log(f);
+
+
+// let d = 5;
+// let s = d--;
+// console.log(d);
+// console.log(s);
+
+// let income = 100;
+// let strIncome = "100";
+// let result = income == strIncome;
+// console.log(result)
+
+
+// let income = 100;
+// let strIncome = "100";
+// let result = income === strIncome;
+// console.log(result)
+
+// let a = 100;
+// let b = 10;
+// let result = a > 50 && b <12;
+// console.log(result)
+
+// let a = 100;
+// let b = true;
+// let result = a > 100 || b == true;
+// console.log(result);
+
